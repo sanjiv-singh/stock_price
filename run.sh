b875a1d24b473b295d67a6f47290432b856f64a7
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-12-05 18:37:55 +0530

    run script on ec2 instance

--- run.sh
+++ run.sh
@@ -4,5 +4,8 @@
 # Obtain IP Addr
 ip_address=$(cat ip_address.txt)
 
-# Run the application in the ec2 instance
+# Copy the application to ec2 instance
+scp -i gl-test.pem -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null *.py ec2-user@$ip_address:.
+
+# Run the application in ec2 instance
 ssh -i gl-test.pem -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null ec2-user@$ip_address "python3 stock_price_ingestion.py"
