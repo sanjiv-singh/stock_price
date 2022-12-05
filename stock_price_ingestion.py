9e8c3561a359b2c623f6c5f82f7a4dd383e59a55
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-12-05 18:38:43 +0530

    push to kinesis datastream

--- stock_price_ingestion.py
+++ stock_price_ingestion.py
@@ -1,11 +1,7 @@
-import json
 import boto3
-import sys
 import yfinance as yf
-
-import time
-import random
-import datetime
+from datetime import date, datetime, timedelta
+from stream import KinesisStream
 
 TICKERS = ['MSFT', 'MVIS', 'GOOG', 'SPOT', 'INO', 'OCGN', 'ABML', 'RLLCF', 'JNJ', 'PSFE']
 
@@ -16,27 +12,50 @@ TICKERS = ['MSFT', 'MVIS', 'GOOG', 'SPOT', 'INO', 'OCGN', 'ABML', 'RLLCF', 'JNJ'
 
 kinesis = boto3.client('kinesis', region_name = "us-east-1") #Modify this line of code according to your requirement.
 
-today = datetime.date.today() - datetime.timedelta(days=2)
+today = date.today() - timedelta(days=2)
 day_of_week = today.weekday()
 if day_of_week < 2:
-    today += datetime.timedelta(2 - day_of_week)
-yesterday = today - datetime.timedelta(1)
+    today += timedelta(2 - day_of_week)
+yesterday = today - timedelta(1)
 
 # Example of pulling the data between 2 dates from yfinance API
 #data = yf.download("MSFT", start=yesterday, end=today, interval='1h' )
 
-# Add code to pull the data for the stocks specified in the doc
-stock_data = {}
-for ticker in TICKERS:
-    data = yf.download(ticker, start=yesterday, end=today, interval='1h' )
-    stock_data[ticker] = data
 
-for ticker, data in stock_data.items():
-    print(f"------------------- {ticker} ----------------")
-    print(data.head())
+def get_hourly_stock_data(stockid, high, low):
+    hourly_stock_data = []
+    data = yf.download(stockid, start=yesterday, end=today, interval='1h')
+    for timestamp in data.index:
+        hourly_data = {}
+        _data = data.loc[timestamp]
+        hourly_data['stockid'] = stockid
+        hourly_data['price'] = _data['Close']
+        hourly_data['price timestamp'] = datetime.strftime(timestamp, "%Y-%m-%d %H:%M:%S")
+        hourly_data['52WeekHigh'] = high
+        hourly_data['52WeekLow'] = low
+        hourly_stock_data.append(hourly_data)
+    return hourly_stock_data
+
+# Add code to pull the data for the stocks specified in the doc
 # Add additional code to call 'info' API to get 52WeekHigh and 52WeekLow refering this this link - https://pypi.org/project/yfinance/
+stock_data = []
+for ticker in TICKERS:
+    _t = yf.Ticker(ticker)
+    high = _t.info.get('fiftyTwoWeekHigh')
+    low = _t.info.get('fiftyTwoWeekLow')
+    stock_data.extend(get_hourly_stock_data(ticker, high, low))
+
+print('\n----------------------- Stock Data --------------------')
+print('STOCK:\tTIMESTAMP\t\tSTOCK PRICE\t52WEEKHIGH\t52WEEKLOW')
+sorted_stock_data = sorted(stock_data, key=lambda d: d['price timestamp']) 
+
+for data in sorted_stock_data:
+    print(f'{data["stockid"]}\t{data["price timestamp"]}\t{data["price"]}\t{data["52WeekHigh"]}\t{data["52WeekLow"]}')
 
 
 ## Add your code here to push data records to Kinesis stream.
+stream = KinesisStream(kinesis, 'gl-stock-price')
+for data in sorted_stock_data:
+    stream.put_record(data, "stockid")
 
 
