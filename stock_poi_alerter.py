2b5224f7ac7234425fca6e25331b921cd548b3e6
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-12-05 23:32:27 +0530

    updated lambda to raise alert only if first alert of day

--- stock_poi_alerter.py
+++ stock_poi_alerter.py
@@ -1,11 +1,9 @@
 import boto3
 import json
 import base64
-import logging
+from boto3.dynamodb.conditions import Key
+from datetime import datetime, timedelta
 from decimal import Decimal
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 db_table = dynamodb.Table('stock-price-poi-alert')
@@ -20,11 +18,30 @@ def send_sns_notification(data):
         Subject=f'Stock Price Alert for {data["stockid"]}',
         MessageStructure='json')
 
-def handle_poi(data):
+def store_data(data):
+
     parsed_data = json.loads(json.dumps(data), parse_float=Decimal)
     response = db_table.put_item(
         Item=parsed_data
     )
+
+def handle_poi(data):
+
+    stockid = data["stockid"]
+    timestamp = datetime.strptime(data["timestamp"], "%Y-%m-%d %H:%M:%S")
+    present_date = timestamp.date()
+    next_date = present_date + timedelta(days=1)
+    start_time = datetime.strftime(present_date, "%Y-%m-%d %H:%M:%S")
+    end_time = datetime.strftime(next_date, "%Y-%m-%d %H:%M:%S")
+
+    query_params = {}
+    query_params['KeyConditionExpression'] = Key('stockid').eq(stockid) & \
+            Key('timestamp').between(start_time, end_time)
+    response = db_table.query(**query_params)
+    pois = response.get('Items')
+    if pois:
+        return None
+    store_data(data)
     send_sns_notification(data)
 
 
